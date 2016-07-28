97d7291eba935fc72b643e6578a2118ddb934ae2
kiraknowsbest <kinaharo@gmail.com>
2016-07-28 10:04:04 -0700

    Make linkedList pass all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,39 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.len = 0;
+  list.headIndex = 0;
 
   list.addToTail = function(value) {
+    list[list.len] = Node( value );
+    list.tail = list[list.len];
+    //if list.len = 0, sets value for list.head
+    if ( list.len === 0 ) {
+      list.head = list.tail;
+      list.headIndex = 0;
+    }
+    list.len++;
   };
 
   list.removeHead = function() {
+    var removedHead = list[list.headIndex].value;
+    delete list[list.headIndex];
+    list.headIndex++;
+    list.head = list[list.headIndex];
+    list.len--;
+    return removedHead;
   };
 
   list.contains = function(target) {
+    // refactored with contains and pluck!
+    return _.contains(_.pluck( list, 'value' ), target);
+      
+    /*for ( var key in list ) {
+      if ( list[key].value === target ) {
+        return true;
+      }
+    }
+    return false;*/
   };
 
   return list;
